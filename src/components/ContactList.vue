86962adf66839c38d0a2a6417d3fb8679e80acb0
Tran Van Luong <tranvanluong032020@gmail.com>
2024-10-01 16:50:38 +0700

    them file da chinh sua

---
+++ src/components/ContactList.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    contacts: { type: Array, default: [] },
+    activeIndex: { type: Number, default: -1 }
+  },
+  emits: ["update: activeIndex"],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit("update:activeIndex", index)
+    }
+  }
+}
+</script>
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" :key="contact.id" v-for="(contact, index) in contacts"
+      :class="{ active: index === activeIndex }" @click="updateActiveIndex(index)">
+      {{ contact.name }}
+    </li>
+  </ul>
+
+</template>
